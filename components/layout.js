8b1ebfd760eb89ba7f390a8356f74c0b903f98ed
Brian Jenkins <brian@jenkinz.com>
2022-09-18 17:47:30 -0700

    Set site title to my name

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Brian Jenkins";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = name;
 
 export default function Layout({ children, home }) {
   return (
